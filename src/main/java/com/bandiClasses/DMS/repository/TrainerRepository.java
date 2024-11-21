4d2fa56ac708eaac225a1f2a24102445a6cb57c4
S571495 <ys.sara2068@gmail.com>
2024-11-21 11:51:29 -0600

    commit

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ *  @author Yashaswini Sarabu 
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer>{
+	
+}
